dbbe65bf331730d9f4bc10af7c31a7e19c16a7e2
Neooo <139463783+Neooo17@users.noreply.github.com>
2023-11-23 18:06:50 +0530

    Update script.js
    doing this by math.js library instead of eval function

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const screen = document.querySelector(".screen");
 let expression = "";
 
 const buttons = document.querySelectorAll(".calc-button");
-buttons.forEach(function (button){
-    button.addEventListener("click", function(){
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
         const buttonText = button.textContent;
-        
+
         if (buttonText === "DEL") {
             expression = expression.slice(0, -1);
-        }else if (buttonText === "RESET") {
+        } else if (buttonText === "RESET") {
             expression = "";
             screen.textContent = "0";
-        }else if (buttonText === "=") {
-            // Replace 'x' with '*' 
-            const result = eval(expression.replace(/×/g, '*'));
-            expression = result;
-        }
-        else{
+        } else if (buttonText === "=") {
+            try {
+                // Replace 'x' with '*'
+                const result = math.evaluate(expression.replace(/×/g, '*'));
+                expression = result.toString();
+            } catch (error) {
+                expression = "Error";
+            }
+        } else {
             expression += buttonText;
         }
         screen.textContent = expression || "0";
@@ -25,23 +29,23 @@ buttons.forEach(function (button){
     })
 })
 
-//theme change.....
+
+
+// Theme change functionality
 
 const slider = document.querySelector(".slider");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
-slider.addEventListener("input", function(){
+slider.addEventListener("input", function () {
     // console.log(this.value);
     const themeValue = this.value;
     if (themeValue === '2') {
         body.classList.add("theme2");
-        body.classList.remove("theme3")
-    }
-    else if (themeValue === '3') {
-        body.classList.add("theme3")
-        body.classList.remove("theme2")
-    }
-    else if (themeValue === '1') {
+        body.classList.remove("theme3");
+    } else if (themeValue === '3') {
+        body.classList.add("theme3");
+        body.classList.remove("theme2");
+    } else if (themeValue === '1') {
         body.classList.remove("theme2");
         body.classList.remove("theme3");
     }
